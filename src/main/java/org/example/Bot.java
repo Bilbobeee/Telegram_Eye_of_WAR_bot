754bbd53c2b85b5709d6be04143916b2383e3b08
Bilbobeee <bilbobeee@gmail.com>
2023-02-24 23:11:19 +0200

    commit #2 register in main Telegram bot

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -5,8 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class Bot extends TelegramLongPollingBot {
 
-    final private String BOT_TOKEN = "";
-    final private String BOT_NAME = "";
+    final private String BOT_TOKEN = "5900361736:AAEBrf8uYxR1H7WVwnRlYegsAmsw_cFuBZk";
+    final private String BOT_NAME = "Eye_of_WAR_bot";
 
     public String getBOT_TOKEN() {
         return BOT_TOKEN;

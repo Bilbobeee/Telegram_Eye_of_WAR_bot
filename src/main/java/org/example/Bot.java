70735cb52ac7d057ed70fe1f33205f01543b0c4f
Bilbobeee <bilbobeee@gmail.com>
2023-02-24 23:35:55 +0200

    commit #3 created out message bot

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,28 +1,42 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
     final private String BOT_TOKEN = "5900361736:AAEBrf8uYxR1H7WVwnRlYegsAmsw_cFuBZk";
     final private String BOT_NAME = "Eye_of_WAR_bot";
 
-    public String getBOT_TOKEN() {
+    public String getBotToken() {
         return BOT_TOKEN;
     }
 
-    public String getBOT_NAME() {
+    @Override
+    public String getBotUsername() {
         return BOT_NAME;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
 
+        Message inMess = update.getMessage();
+        String chatId = inMess.getChatId().toString();
+
+
+        SendMessage outMess = new SendMessage();
+        outMess.setChatId(chatId);
+        outMess.setText("TEST Mess");
+        try {
+            execute(outMess);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+
     }
 
-    @Override
-    public String getBotUsername() {
-        return null;
-    }
+
 }
